2446821fb1fe33a9098e123ce4ff3fc69abcabc6
idev <jic4656@naver.com>
2020-06-15 13:52:21 +0900

    Add build

---
+++ frontend/snap_plan_manager/build/precache-manifest.d287cc5e126d932db074531350c7b725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041294805c11486314525339f80d821b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10a2dce0875ec7c7b017",
+    "url": "/static/css/main.90adbb45.chunk.css"
+  },
+  {
+    "revision": "f2faf6536abaf3dbd3ae",
+    "url": "/static/js/2.899c2771.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.899c2771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a2dce0875ec7c7b017",
+    "url": "/static/js/main.1bea7a9b.chunk.js"
+  },
+  {
+    "revision": "94a2454e58b8c2262752",
+    "url": "/static/js/runtime-main.d213a5ff.js"
+  }
+]);
